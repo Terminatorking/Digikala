55909d3f227ba4349507abb45900a40bf53aaa8c
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-19 17:27:24 +0330

    add project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,35 +50,41 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     //Material2
-    implementation("androidx.compose.material:material:1.8.3")
-//    //Retrofit
-//    implementation ("com.squareup.retrofit2:retrofit:3.0.0")
-//    implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
-//    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-//    implementation ("com.google.code.gson:gson:2.10.1")
-//    //Room database
-//    implementation ("androidx.room:room-runtime:$room_version")
-//    kapt ("androidx.room:room-compiler:$room_version")
-//    implementation ("androidx.room:room-ktx:$room_version")
-//    //Datastore
-//    implementation ("androidx.datastore:datastore-preferences:$datastore_version")
-//    //Hilt
-//    implementation ("com.google.dagger:hilt-android:$hilt_version")
-//    kapt ("com.google.dagger:hilt-compiler:$hilt_version")
-//    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
-//    //Compose Navigation
-//    implementation ("androidx.navigation:navigation-compose:2.5.3")
-//    //Animation
-//    implementation ("com.airbnb.android:lottie-compose:5.2.0")
-//    //Coil Load Image From Url
-//    implementation ("io.coil-kt:coil-compose:2.3.0")
-//    //Swipe refresh
-//    implementation ("com.google.accompanist:accompanist-swiperefresh:0.30.0")
-//    //System Ui Controller
-//    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
-//    //Accompanist-Pager
-//    implementation ("com.google.accompanist:accompanist-pager:0.30.0")
-//    implementation ("com.google.accompanist:accompanist-pager-indicators:0.30.0")
+    implementation(libs.androidx.material)
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.gson)
+    //Room database
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    //Datastore
+    implementation(libs.androidx.datastore.preferences)
+    //Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    //Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+    //Animation
+    implementation(libs.lottie.compose)
+    //Coil Load Image From Url
+    implementation(libs.coil.compose)
+    //Swipe refresh
+    implementation(libs.accompanist.swiperefresh)
+    //System Ui Controller
+    implementation(libs.accompanist.systemuicontroller)
+    //Accompanist-Pager
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.pager.indicators)
+    //paging3
+    implementation(libs.androidx.paging.compose)
+    //chart
+    implementation(libs.patrykandpatrick.chart)
+    //icon
+    implementation(libs.androidx.material.icons.extended)
 }
 
 kapt {
--- app/src/main/java/ghazimoradi/soheil/digikala/ui/theme/Color.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package ghazimoradi.soheil.digikala.ui.theme
 
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
@@ -11,7 +12,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
 val Purple200 = Color(0xFFed1b34)
 val Purple500 = Color(0xFFCE3C4D)
 val Purple700 = Color(0xFF3700B3)
