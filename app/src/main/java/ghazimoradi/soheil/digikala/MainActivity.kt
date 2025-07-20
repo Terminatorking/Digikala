86c18216a8a4136f2d6e31a494bd351e73043fdf
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-20 17:27:15 +0330

    handle app locale

--- app/src/main/java/ghazimoradi/soheil/digikala/MainActivity.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/MainActivity.kt
@@ -5,11 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import ghazimoradi.soheil.digikala.navigation.BottomNavigationBar
 import ghazimoradi.soheil.digikala.navigation.SetupNavGraph
 import ghazimoradi.soheil.digikala.ui.theme.DigikalaTheme
+import ghazimoradi.soheil.digikala.util.LocaleUtils
+import ghazimoradi.soheil.digikala.util.Constants.PERSIAN_LANG
 
 class MainActivity : ComponentActivity() {
 
@@ -20,18 +26,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             DigikalaTheme {
                 navController = rememberNavController()
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(
-                            navController = navController,
-                            onItemClick = { bottomNavItem ->
-                                navController.navigate(bottomNavItem.route)
-                            },
-                        )
-                    },
-                ) { padding ->
-                    SetupNavGraph(navController)
-                    Log.i("padding", padding.toString())
+                LocaleUtils.setLocale(LocalContext.current, PERSIAN_LANG)
+
+                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(
+                                navController = navController,
+                                onItemClick = { bottomNavItem ->
+                                    navController.navigate(bottomNavItem.route)
+                                },
+                            )
+                        },
+                    ) { padding ->
+                        SetupNavGraph(navController)
+                        Log.i("padding", padding.toString())
+                    }
                 }
             }
         }
