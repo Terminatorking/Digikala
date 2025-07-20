730581c8e52bcfb7bfa32d65ac3076d44a31e4f7
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-20 16:41:48 +0330

    add project typographies

--- app/src/main/java/ghazimoradi/soheil/digikala/ui/theme/Type.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/theme/Type.kt
@@ -1,34 +1,96 @@
 package ghazimoradi.soheil.digikala.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ghazimoradi.soheil.digikala.R
 
-// Set of Material typography styles to start with
+val font_medium = FontFamily(
+    Font(R.font.iranyekanmedium)
+)
+val font_bold = FontFamily(
+    Font(R.font.iranyekanbold)
+)
+val font_standard = FontFamily(
+    Font(R.font.iranyekan)
+)
+
+val Typography.extraBoldNumber : TextStyle
+    @Composable
+    get() = TextStyle(
+        fontFamily = font_bold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+    )
+
+val Typography.extraSmall : TextStyle
+    @Composable
+    get() = TextStyle(
+        fontFamily = font_standard,
+        fontSize = 11.sp,
+        lineHeight = 25.sp
+    )
+
+val Typography.veryExtraSmall : TextStyle
+    @Composable
+    get() = TextStyle(
+        fontFamily = font_standard,
+        fontSize = 10.sp,
+    )
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    body1 = TextStyle(
+        fontFamily = font_medium,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 25.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 25.sp
+    ),
+
+    h1 = TextStyle(
+        fontFamily = font_standard,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 20.sp,
+        lineHeight = 25.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 25.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 25.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        lineHeight = 25.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 25.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 25.sp
     )
-    */
 )
