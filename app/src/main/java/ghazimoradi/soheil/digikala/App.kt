3740818e1ad0fb3d7c8daa35b3d17376c53d7161
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-20 20:42:23 +0330

    change app language using bottoms

--- app/src/main/java/ghazimoradi/soheil/digikala/App.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/App.kt
@@ -1,7 +1,12 @@
 package ghazimoradi.soheil.digikala
 
+import android.app.Activity
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    companion object {
+       lateinit var currentActitvity: Activity
+    }
+}
--- app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/ProfileScreen.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/ProfileScreen.kt
@@ -1,10 +1,59 @@
 package ghazimoradi.soheil.digikala.ui.screens
 
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import ghazimoradi.soheil.digikala.App
+import ghazimoradi.soheil.digikala.MainActivity
+import ghazimoradi.soheil.digikala.viewmodel.DataStoreViewModel
+import ghazimoradi.soheil.digikala.util.Constants.PERSIAN_LANG
+import ghazimoradi.soheil.digikala.util.Constants.ENGLISH_LANG
 
 @Composable
-fun ProfileScreen(navController: NavHostController){
-    Text("ProfileScreen")
+fun ProfileScreen(
+    navController: NavHostController,
+    dataStoreViewModel: DataStoreViewModel = hiltViewModel()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        val activity = App.currentActitvity
+        Text("ProfileScreen")
+        Button(
+            onClick = {
+                dataStoreViewModel.saveUserLanguage(PERSIAN_LANG)
+                activity.apply {
+                    finish()
+                    startActivity(Intent(activity, MainActivity::class.java))
+                }
+            },
+        ) {
+            Text("fa")
+        }
+        Button(
+            onClick = {
+                dataStoreViewModel.saveUserLanguage(ENGLISH_LANG)
+                activity.apply {
+                    finish()
+                    startActivity(Intent(activity, MainActivity::class.java))
+                }
+            },
+        ) {
+            Text("en")
+        }
+    }
 }
