f2139bd8a3ba1060bdff2c4db9a41f6d61f4ba55
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-22 16:31:04 +0330

    refactoring home screen

--- app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/home/HomeScreen.kt
@@ -1,22 +1,17 @@
 package ghazimoradi.soheil.digikala.ui.screens.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
-import ghazimoradi.soheil.digikala.ui.theme.White
 import ghazimoradi.soheil.digikala.viewmodel.HomeViewModel
 import kotlinx.coroutines.launch
 
@@ -31,30 +26,38 @@ fun Home(
     navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(true) {
+        refreshDataFromServer(viewModel = homeViewModel)
+    }
+
+    SwipeRefreshSection(viewModel = homeViewModel, navController = navController)
+}
+
+@Composable
+@Suppress("Deprecation")
+fun SwipeRefreshSection(viewModel: HomeViewModel, navController: NavController) {
     val refreshScope = rememberCoroutineScope()
     val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
-
     SwipeRefresh(
         swipeRefreshState,
         onRefresh = {
             refreshScope.launch {
-
+                refreshDataFromServer(viewModel = viewModel)
             }
         }
     ) {
-        Column(
-            modifier = Modifier
-                .background(White)
-                .padding(bottom = 60.dp)
-                .verticalScroll(rememberScrollState())
-                .fillMaxSize()
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            item {
+                SearchBarSection()
 
-        ) {
-            LaunchedEffect(true) {
-                homeViewModel.getAllDataFromServer()
             }
-            SearchBarSection()
-            TopSliderSection()
+            item {
+                TopSliderSection()
+            }
         }
     }
+}
+
+private fun refreshDataFromServer(viewModel: HomeViewModel) {
+    viewModel.getAllDataFromServer()
 }
