fa07065fe7a5235e14bfae8fa36b64c765c39b08
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-24 15:40:52 +0330

    handle basket screen repository and view model

--- app/src/main/java/ghazimoradi/soheil/digikala/repository/BasketRepository.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/repository/BasketRepository.kt
@@ -1,11 +1,54 @@
 package ghazimoradi.soheil.digikala.repository
 
+import ghazimoradi.soheil.digikala.data.db.CartDao
+import ghazimoradi.soheil.digikala.data.model.basket.CartItem
+import ghazimoradi.soheil.digikala.data.model.basket.CartStatus
+import ghazimoradi.soheil.digikala.data.model.home.StoreProduct
 import ghazimoradi.soheil.digikala.data.remote.BaseApiResponse
 import ghazimoradi.soheil.digikala.data.remote.BasketApiInterface
+import ghazimoradi.soheil.digikala.data.remote.NetworkResult
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class BasketRepository @Inject constructor(
-    val api: BasketApiInterface
+    private val api: BasketApiInterface,
+    private val dao: CartDao
 ) : BaseApiResponse() {
 
+    val currentCartItems = dao.getAllItems(CartStatus.CURRENT_CART)
+    val nextCartItems = dao.getAllItems(CartStatus.NEXT_CART)
+
+    val currentCartItemsCount = dao.getCartItemsCount(CartStatus.CURRENT_CART)
+    val nextCartItemsCount = dao.getCartItemsCount(CartStatus.NEXT_CART)
+
+    suspend fun getSuggestedItems(): NetworkResult<List<StoreProduct>> =
+        safeApiCall {
+            api.getSuggestedItems()
+        }
+
+    suspend fun insertCartItem(cart: CartItem) {
+        dao.insertCartItem(cart)
+    }
+
+    suspend fun removeFromCart(cart: CartItem) {
+        dao.removeFromCart(cart)
+    }
+
+    fun deleteAllItems() {
+        dao.deleteAllItems(CartStatus.CURRENT_CART)
+    }
+
+    suspend fun changeCartItemStatus(id: String, newStatus: CartStatus) {
+        dao.changeStatusCart(id, newStatus)
+    }
+
+    suspend fun changeCartItemCount(id: String, newCount: Int) {
+        dao.changeCountCartItem(id, newCount)
+    }
+
+    fun getItemsCountInBasket(itemId: String): Flow<Int> =
+        dao.getItemsCountInBasket(itemId)
+
+    fun isItemExistInBasket(itemId: String): Flow<Boolean> =
+        dao.isItemExistInBasket(itemId)
 }
