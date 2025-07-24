1db1b4a63ecedcf95049dbde12dc2ca0a8afb659
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-07-24 15:44:58 +0330

    handle basket empty

--- app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/basket/ShoppingCart.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/basket/ShoppingCart.kt
@@ -1,7 +1,15 @@
 package ghazimoradi.soheil.digikala.ui.screens.basket
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import ghazimoradi.soheil.digikala.viewmodel.BasketViewModel
@@ -11,5 +19,21 @@ fun ShoppingCart(
     navController: NavController,
     viewModel: BasketViewModel = hiltViewModel()
 ) {
-    Text("Shopping Cart")
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(bottom = 60.dp),
+        ) {
+            item {
+                EmptyBasketShopping()
+            }
+        }
+    }
 }
