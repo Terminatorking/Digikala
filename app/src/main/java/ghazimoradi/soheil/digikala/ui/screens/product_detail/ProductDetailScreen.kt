1f77f9c2ccd641f741bfc4f8364a470f8a5103c0
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2025-08-03 17:27:54 +0330

    handle product detail screen app bar

--- app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/product_detail/ProductDetailScreen.kt
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/product_detail/ProductDetailScreen.kt
@@ -1,8 +1,11 @@
 package ghazimoradi.soheil.digikala.ui.screens.product_detail
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +26,7 @@ import ghazimoradi.soheil.digikala.data.model.product_detail.SliderImage
 import ghazimoradi.soheil.digikala.data.remote.NetworkResult
 import ghazimoradi.soheil.digikala.ui.components.OurLoading
 import ghazimoradi.soheil.digikala.ui.components.getScreenHeight
+import ghazimoradi.soheil.digikala.ui.theme.mainBg
 import ghazimoradi.soheil.digikala.viewmodel.ProductDetailViewModel
 import kotlinx.coroutines.flow.collectLatest
 
@@ -33,7 +37,7 @@ fun ProductDetailScreen(
     viewModel: ProductDetailViewModel = hiltViewModel(),
 ) {
 
-    var productDetailList by remember {
+    var productDetail by remember {
         mutableStateOf(ProductDetail())
     }
 
@@ -64,24 +68,24 @@ fun ProductDetailScreen(
 
     LaunchedEffect(true) {
         viewModel.getProductById(productId)
-        viewModel.productDetail.collectLatest { productDetail ->
-            when (productDetail) {
+        viewModel.productDetail.collectLatest { netWorkProductDetail ->
+            when (netWorkProductDetail) {
                 is NetworkResult.Success -> {
-                    productDetailList = productDetail.data!!
-                    imageSlider = productDetail.data.imageSlider ?: emptyList()
-                    productColors = productDetail.data.colors ?: emptyList()
-                    productComments = productDetail.data.comments ?: emptyList()
-                    productPriceList = productDetail.data.priceList ?: emptyList()
-                    categoryId = productDetail.data.categoryId ?: ""
-                    description = productDetail.data.description ?: ""
-                    commentCount = productDetail.data.commentCount ?: 0
-                    technicalFeatures = productDetail.data.technicalFeatures.toString()
+                    productDetail = netWorkProductDetail.data!!
+                    imageSlider = netWorkProductDetail.data.imageSlider ?: emptyList()
+                    productColors = netWorkProductDetail.data.colors ?: emptyList()
+                    productComments = netWorkProductDetail.data.comments ?: emptyList()
+                    productPriceList = netWorkProductDetail.data.priceList ?: emptyList()
+                    categoryId = netWorkProductDetail.data.categoryId ?: ""
+                    description = netWorkProductDetail.data.description ?: ""
+                    commentCount = netWorkProductDetail.data.commentCount ?: 0
+                    technicalFeatures = netWorkProductDetail.data.technicalFeatures.toString()
                     loading = false
                 }
 
                 is NetworkResult.Error -> {
                     loading = false
-                    Log.e("3636", "ProductDetailScreen error : ${productDetail.message}")
+                    Log.e("3636", "ProductDetailScreen error : ${netWorkProductDetail.message}")
                 }
 
                 is NetworkResult.Loading -> {
@@ -96,15 +100,19 @@ fun ProductDetailScreen(
     } else {
         Scaffold(
             bottomBar = {
-                ProductDetailBottomBar(productDetailList, navController)
+                ProductDetailBottomBar(productDetail, navController)
             },
 
             topBar = {
-
+                ProductTopAppBar(navController = navController, product = productDetail)
             }
         ) { padding ->
             Log.i("padding", padding.toString())
-            LazyColumn(modifier = Modifier.padding(bottom = 70.dp)) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+                    .padding(bottom = 70.dp)
+                    .background(MaterialTheme.colors.mainBg)
+            ) {
 
             }
         }
---
+++ app/src/main/java/ghazimoradi/soheil/digikala/ui/screens/product_detail/ProductTopAppBar.kt
@@ -0,0 +1,194 @@
+package ghazimoradi.soheil.digikala.ui.screens.product_detail
+
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import ghazimoradi.soheil.digikala.R
+import ghazimoradi.soheil.digikala.data.model.prfile.FavItem
+import ghazimoradi.soheil.digikala.data.model.product_detail.ProductDetail
+import ghazimoradi.soheil.digikala.navigation.Screen
+import ghazimoradi.soheil.digikala.ui.theme.darkText
+import ghazimoradi.soheil.digikala.ui.theme.icon
+import ghazimoradi.soheil.digikala.ui.theme.spacing
+import ghazimoradi.soheil.digikala.util.DigitHelper.digitByLocateAndSeparator
+
+@Composable
+fun ProductTopAppBar(navController: NavHostController, product: ProductDetail) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .padding(horizontal = MaterialTheme.spacing.small),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier.weight(0.6f),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            IconButton(
+                onClick = {
+                    navController.popBackStack()
+                },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.exit),
+                    contentDescription = "",
+                    modifier = Modifier.size(17.dp),
+                    tint = MaterialTheme.colors.icon
+                )
+            }
+        }
+
+        Row(modifier = Modifier.weight(0.4f)) {
+
+            IconButton(
+                onClick = {
+                    navController.navigate(Screen.Basket.route)
+                },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.basket),
+                    contentDescription = "",
+                    modifier = Modifier.size(25.dp),
+                    tint = MaterialTheme.colors.icon
+                )
+            }
+
+            DisplayFavoriteToggleButton(
+                FavItem(
+                    product._id ?: "",
+                    product.discountPercent ?: 0,
+                    product.imageSlider?.get(0)?.image ?: "",
+                    product.name ?: "",
+                    product.price ?: 0,
+                    product.seller ?: "",
+                    product.star ?: 0.0
+                )
+            )
+
+
+            var expanded by remember { mutableStateOf(false) }
+            IconButton(
+                onClick = {
+                    expanded = true
+                },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.menu_dots),
+                    contentDescription = "",
+                    modifier = Modifier.size(27.dp),
+                    tint = MaterialTheme.colors.icon
+                )
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.background(MaterialTheme.colors.surface)
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+//                        val priceListString = Gson().toJson(product.priceList)
+//                        navController.navigate(
+//                            Screen.ProductPriceChart.route + "?jsonString=${priceListString}"
+//                        )
+                    }
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = MaterialTheme.spacing.extraSmall),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.chart),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(16.dp),
+                            tint = MaterialTheme.colors.icon
+                        )
+
+                        Spacer(modifier = Modifier.width(MaterialTheme.spacing.small))
+
+                        Text(
+                            text = stringResource(R.string.price_chart),
+                            style = MaterialTheme.typography.h4,
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colors.darkText,
+                        )
+                    }
+                }
+
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        shareToSocialMedia(
+                            context,
+                            product.name!!,
+                            digitByLocateAndSeparator(product.price!!.toString()),
+                            "https://truelearn.ir/"
+                        )
+                    }
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = MaterialTheme.spacing.extraSmall),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.share),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(16.dp),
+                            tint = MaterialTheme.colors.icon
+                        )
+
+                        Spacer(modifier = Modifier.width(MaterialTheme.spacing.small))
+
+                        Text(
+                            text = stringResource(R.string.share_product),
+                            style = MaterialTheme.typography.h4,
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colors.darkText,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun shareToSocialMedia(
+    context: Context,
+    productName: String,
+    productPrice: String,
+    url: String
+) {
+    val shareIntent = Intent(Intent.ACTION_SEND)
+    shareIntent.type = "text/plain"
+
+    shareIntent.putExtra(
+        Intent.EXTRA_TEXT,
+        "$productName با قیمت باورنکردنی $productPrice تومان فقط در فروشگاه زیر \n $url"
+    )
+
+    context.startActivity(Intent.createChooser(shareIntent, "share to..."))
+}
